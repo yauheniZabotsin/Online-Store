acc1fc9feafa3e66187b2f1e253df698fe262667
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2022-12-27 01:18:29 +0300

    feat: add product data

--- src/pages/product/product.ts
+++ src/pages/product/product.ts
@@ -1,5 +1,6 @@
 import Page from "../../core/templates/page";
 import "../product/product.css";
+import prodData from '../../components/data/data';
 
 class ProductPage extends Page {
   static TextObject = {
@@ -11,10 +12,41 @@ class ProductPage extends Page {
   }
 
   render(): HTMLElement {
-    const productDetails = document.createElement("div");
-    productDetails.className = "productt-wrap"
-    const title = this.createHeaderTitle(ProductPage.TextObject.ProductTitle);
-    this.container.append(title);
+    const productWrap = document.createElement("div");
+    this.container.append(productWrap);
+
+    const linkNavigation = document.createElement("div");
+    linkNavigation.className = "link-navigation";
+    for(let i=0; i<4; i++){
+      const link = document.createElement("a");
+      linkNavigation.appendChild(link);
+    }
+    this.container.append(linkNavigation);
+
+    const productDetail = document.createElement("div");
+    productDetail.className = "product-detail";
+    const productTitle = document.createElement("h2");
+    productTitle.className = "product-title";
+    productTitle.textContent = prodData.products[0].title;
+    productDetail.appendChild(productTitle);
+    this.container.append(productDetail);
+
+    const productData = document.createElement("div");
+    productData.className = "product-data";
+    const productPhotos = document.createElement("div");
+    productPhotos.className = "product-photos";
+    const slides = document.createElement("div");
+    slides.className = "slides";
+    for(let i = 1; i < 4; i++){
+      const img = document.createElement("img");
+      img.alt = "slide";
+      img.src = prodData.products[0].images[i];
+      slides.append(img);
+    }
+    productPhotos.append(slides);
+    productData.append(productPhotos);
+    this.container.append(productData);
+
     return this.container;
   }
 
