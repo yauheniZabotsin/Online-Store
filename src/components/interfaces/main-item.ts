e42ee187a8caa096ca726a5abeef806100d60c64
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2022-12-26 18:02:34 +0300

    refactor: add height item-buttons

--- src/components/interfaces/main-item.ts
+++ src/components/interfaces/main-item.ts
@@ -469,17 +469,22 @@ filtersProd = `<div class="filters">
   >
   <div class="category">
       <h3 class="filter-title">Price</h3>
-      <div class="out-data">
-          <div class="from-data">€ <input class="form_control_container__time__input" type="number" id="fromInput" value="10" min="0" max="100"/></div>
-          <!---->
-          ⟷
-          <div class="to-data">€ <input class="form_control_container__time__input" type="number" id="toInput" value="40" min="0" max="100"/></div>
-          <!---->
+      <div class="range_container">
+        <div class="sliders_control">
+          <input id="fromSlider" type="range" value="0" min="0" max="100"/>
+          <input id="toSlider" type="range" value="100" min="0" max="100"/>
+        </div>
+      <div class="form_control">
+        <div class="form_control_container">
+            <div class="form_control_container__time">€</div>
+            <input class="form_control_container__time__input" type="number" id="fromInput" value="0" min="0" max="100"/>
+        </div>
+        <div class="form_control_container">
+            <div class="form_control_container__time">€</div>
+            <input class="form_control_container__time__input" type="number" id="toInput" value="100" min="0" max="100"/>
+        </div>
       </div>
-      <div class="multi-range">
-          <input type="range" min="0" max="48" id="fromSlider"/>
-          <input type="range" min="0" max="48" id="toSlider"/>
-      </div>
+</div>
   </div>
   <div class="category">
       <h3 class="filter-title">Stock</h3>
