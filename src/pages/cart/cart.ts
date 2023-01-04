d1046ec7c00d4994dbf48f557fcf3728b1a51696
yauheniZabotsin <yauheni.zabotsin@gmail.com>
2023-01-04 17:34:16 +0300

    feat: add prod to cart

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -14,8 +14,7 @@ class CartPage extends Page {
   }
 
   render() {
-    console.log("id",CartPage.Products[0])
-    console.log("products[0]",prodData.products[0].id)
+    console.log("id",prodData.products[CartPage.Products[0] - 1].id)
     if(CartPage.Products.length > 0){
       const cartWrapper = document.createElement("div");
       cartWrapper.className = "cart-wrapper";
@@ -42,7 +41,7 @@ class CartPage extends Page {
         const itemInfo = document.createElement("div");
         itemInfo.className = "item-info";
         const itemImg = document.createElement("img");
-        itemImg.src = "https://i.dummyjson.com/data/products/1/thumbnail.jpg"; // надо вставить 
+        itemImg.src = prodData.products[CartPage.Products[i] - 1].thumbnail; // надо вставить 
         itemInfo.append(itemImg);
         const itemDetailP = document.createElement("div");
         itemDetailP.className = "item-detail-p"
@@ -51,22 +50,22 @@ class CartPage extends Page {
         const productTitle = document.createElement("div");
         productTitle.className = "product-title";
         const h3 = document.createElement("h3");
-        h3.textContent = `iPhone 9`; //надо вставить 
+        h3.textContent = prodData.products[CartPage.Products[i] - 1].title; //надо вставить 
         productTitle.append(h3)
         itemDetailP.append(productTitle);
 
         const productDescription = document.createElement("div");
         productDescription.className = "product-description"
-        productDescription.textContent = ` An apple mobile which is nothing like apple `
+        productDescription.textContent = ` ${prodData.products[CartPage.Products[i] - 1].description} ` //надо всавить 
         itemDetailP.append(productDescription);
 
         const productOther = document.createElement("div");
         productOther.className = "product-other"
         const div1 = document.createElement("div");
-        div1.textContent = `Rating: 4.69` //надо вставить 
+        div1.textContent = `Rating: ${prodData.products[CartPage.Products[i] - 1].rating} ` //надо вставить 
         productOther.append(div1);
         const div2 = document.createElement("div");
-        div2.textContent = `Discount: 12.96%` //надо вставить 
+        div2.textContent = `Discount: ${prodData.products[CartPage.Products[i] - 1].discountPercentage}%` //надо вставить 
         productOther.append(div2);
 
         itemDetailP.append(productOther);
@@ -77,7 +76,7 @@ class CartPage extends Page {
 
         const stockControl = document.createElement("div");
         stockControl.className = "stock-control";
-        stockControl.textContent = ` Stock: 94 ` //надо вставить 
+        stockControl.textContent = `Stock: ${prodData.products[CartPage.Products[i] - 1].stock} ` //надо вставить 
         numberControl.append(stockControl);
 
         const incDecControl = document.createElement("div");
@@ -98,7 +97,7 @@ class CartPage extends Page {
 
         const amountControl = document.createElement("div");
         amountControl.className = "amount-control";
-        amountControl.textContent = ` €549.00 `//надо вставить 
+        amountControl.textContent = ` €${prodData.products[CartPage.Products[i] - 1].price} `//надо вставить 
         numberControl.append(amountControl);
 
         cartItem.append(numberControl);
